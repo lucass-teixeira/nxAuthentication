438c73976c642db6fe1082cf954082201550c1c6
Lucas Teixeira <lucasfelician326@gmail.com>
2024-07-29 22:37:39 -0300

    update

--- mobileWebClient/module-federation.config.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleFederationConfig } from '@nx/webpack';
-const config: ModuleFederationConfig = {
-  name: 'mobileWebClient',
-  exposes: {
-    './Routes': 'mobileWebClient/src/app/remote-entry/entry.routes.ts',
-    './mobile-web-client': 'apps/mobile-web-client/src/app/app.routes.ts',
-  },
-};
-export default config;
