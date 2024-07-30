a9061abccc8aa88f20219529bd2338c68cbe450e
Lucas Teixeira <lucasfelician326@gmail.com>
2024-07-29 21:32:30 -0300

    ionic project added and landing

--- apps/mobile-web-client/src/app/app.component.spec.ts
+++ apps/mobile-web-client/src/app/app.component.spec.ts
@@ -1,27 +1,19 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome mobile-web-client'
-    );
-  });
-
-  it(`should have as title 'mobile-web-client'`, () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mobile-web-client');
+    expect(app).toBeTruthy();
   });
 });
